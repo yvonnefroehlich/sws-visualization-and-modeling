e47569021601a87518cc6a5a65eb2ac85d72c8be
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-07-03 07:55:27 +0200

    restructur references and add ORCID

--- 003_modeling/SWS_modeling_check_matlab_version.m
+++ 003_modeling/SWS_modeling_check_matlab_version.m
@@ -6,11 +6,15 @@ function vers_out = SWS_modeling_check_matlab_version()
 % checks the MATLAB version available on your system
 %--------------------------------------------------------------------------
 % is
-% - created: Michael Grund
-%   Grund PhD (2019), Grund & Ritter (2020)
+% - created: Michael Grund (ORCID 0000-0001-8759-2018)
 %   https://github.com/michaelgrund/sws_tools
-% - extended: Yvonne Fröhlich
-%   ORCID 0000-0002-8566-0619
+%   Grund PhD (2019)
+%   https://doi.org/10.5445/IR/1000091425
+%   Grund & Ritter (2020) GJI
+%   https://doi.org/10.1093/gji/ggaa388
+% - modified: Yvonne Fröhlich (ORCID 0000-0002-8566-0619)
+%   https://github.com/yvonnefroehlich
+%   Ritter, Fröhlich, Sanz Alonso & Grund (2022) Journal of Seismology
 %==========================================================================
 
 
@@ -20,7 +24,7 @@ vers_yyyy = str2double(vers(1:4));
 
 % MATLAB R2020a or higher
 if vers_yyyy>2019
-   vers_out=1;
+    vers_out=1;
 else
     vers_out=0;
 end
