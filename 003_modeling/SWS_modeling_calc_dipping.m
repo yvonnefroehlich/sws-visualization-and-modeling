ba4873a548df6e980ed5c7fe47caa96aedc3f487
yvonnefroelich <yvonne.froehlich@kit.edu>
2022-07-20 20:51:16 +0200

    adjust references

--- 003_modeling/SWS_modeling_calc_dipping.m
+++ 003_modeling/SWS_modeling_calc_dipping.m
@@ -19,7 +19,7 @@ function [fast_eff, tlag_eff, azi4plot, fast4plot, tlag4plot] = ...
 %   https://github.com/michaelgrund/sws_tools
 %   Grund PhD (2019)
 %   https://doi.org/10.5445/IR/1000091425
-%   Grund & Ritter (2020) GJI
+%   Grund & Ritter (2020) Geophysical Journal International
 %   https://doi.org/10.1093/gji/ggaa388
 % - lightly modified: Yvonne Fröhlich (ORCID 0000-0002-8566-0619)
 %   https://github.com/yvonnefroehlich/sws-visualization-and-modeling
--- 003_modeling/SWS_modeling_plot_results.m
+++ 003_modeling/SWS_modeling_plot_results.m
@@ -26,7 +26,7 @@ function SWS_modeling_plot_results( ...
 %   https://github.com/michaelgrund/sws_tools
 %   Grund PhD (2019)
 %   https://doi.org/10.5445/IR/1000091425
-%   Grund & Ritter (2020) GJI
+%   Grund & Ritter (2020) Geophysical Journal International
 %   https://doi.org/10.1093/gji/ggaa388
 % - strongly modified and extended: Yvonne Fröhlich (ORCID 0000-0002-8566-0619)
 %   https://github.com/yvonnefroehlich/sws-visualization-and-modeling
