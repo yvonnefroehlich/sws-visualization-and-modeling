f25fffc2f1dd6cf4a23a4229db316868fd7dfed7
yvonnefroelich <yvonne.froehlich@kit.edu>
2024-07-09 16:24:54 +0200

    Improve header of scripts

--- 002_visualization/01_load_radar4kit_data/load_radar4kit_data.py
+++ 002_visualization/01_load_radar4kit_data/load_radar4kit_data.py
@@ -1,21 +1,23 @@
 # #############################################################################
 # Load CSV files with shear wave splitting measurements (SWSMs)
-#
-# Related to the publications
-# (1) Ritter J. R. R., Fröhlich Y., Sanz Alonso Y. & Grund M. (2022).
-#     Short-scale laterally varying SK(K)S shear wave splitting at BFO,
-#     Germany – implications for the determination of anisotropic structures.
-#     Journal of Seismology, 26, 1137-1156.
-#     https://doi.org/10.1007/s10950-022-10112-w.
-# (2) Fröhlich Y., Grund M., Ritter J. R. R. (2024).
-#     Lateral and vertical variations of seismic anisotropy in the
-#     lithosphere-asthenosphere system underneath Central Europe from
-#     long-term splitting measurements. Geophysical Journal International
-#     accepted June 27 2024.
-#
+# -----------------------------------------------------------------------------
 # Data is available from RADAR4KIT
-# (1) https://dx.doi.org/10.35097/684
-# (2) https://dx.doi.org/10.35097/685
+# - Upper Rhine Graben area: https://dx.doi.org/10.35097/685
+#   related to Fröhlich et al. (2024)
+# - Blackforest Observatory: https://dx.doi.org/10.35097/684
+#   related to Ritter et al. (2022)
+# -----------------------------------------------------------------------------
+# Related to the publications
+# - Fröhlich Y., Grund M., Ritter J. R. R. (2024).
+#   Lateral and vertical variations of seismic anisotropy in the
+#   lithosphere-asthenosphere system underneath Central Europe from
+#   long-term splitting measurements. Geophysical Journal International,
+#   accepted June 27 2024.
+# - Ritter J. R. R., Fröhlich Y., Sanz Alonso Y. & Grund M. (2022).
+#   Short-scale laterally varying SK(K)S shear wave splitting at BFO,
+#   Germany – implications for the determination of anisotropic structures.
+#   Journal of Seismology, 26, 1137-1156.
+#   https://doi.org/10.1007/s10950-022-10112-w.
 # -----------------------------------------------------------------------------
 # created: 2024/07/08
 # author: Yvonne Fröhlich
