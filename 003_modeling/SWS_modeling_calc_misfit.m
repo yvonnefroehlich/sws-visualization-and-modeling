3dd0b4f533cd98faeccace7ad0765656b2a558b9
Yvonne Fröhlich <yvonne.froehlich@kit.edu>
2026-01-31 21:59:41 +0100

    Fix setting up gray colormap for RMSE

--- 003_modeling/SWS_modeling_calc_misfit.m
+++ 003_modeling/SWS_modeling_calc_misfit.m
@@ -254,12 +254,12 @@ if cmap_rms_ind==0
     cmap_rms_str = 'rmsno';
 elseif cmap_rms_ind==1
     % built-in MATLAB
-    cmap_rms = flipud( gray(30) );
+    cmap_rms = flipud( gray(plot_mod_max + 10) );
     cmap_rms_str = 'rmsgray';
 elseif cmap_rms_ind==2
     % grayC of Scientific Colormaps version 7.0.1 by Crameri 2021
     % Flip colormap by using '-' before the colormap name
-    cmap_rms = crameri('-grayC', 30);
+    cmap_rms = crameri('-grayC', plot_mod_max + 10);
     cmap_rms_str = 'rmsgrayC';
 end
 
