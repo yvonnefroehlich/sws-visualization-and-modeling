8ce926c107da3b3576b7619774a348ed7f94816e
Yvonne Fröhlich <94163266+yvonnefroehlich@users.noreply.github.com>
2026-01-31 16:28:52 +0100

    General: Clean code part I (#46)

--- 003_modeling/SWS_modeling_check_matlab_version.m
+++ 003_modeling/SWS_modeling_check_matlab_version.m
@@ -5,7 +5,7 @@ function vers_out = SWS_modeling_check_matlab_version()
 % ==========================================================================
 % checks the MATLAB version available on your system
 % --------------------------------------------------------------------------
-% is 
+% is
 % - modified: Yvonne Fröhlich (ORCID 0000-0002-8566-0619)
 %   https://github.com/yvonnefroehlich/sws-visualization-and-modeling
 %   Fröhlich (2025) Dissertation
